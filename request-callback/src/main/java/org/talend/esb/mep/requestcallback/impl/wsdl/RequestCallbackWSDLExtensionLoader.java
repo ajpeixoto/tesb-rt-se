24a0fb5710feef82f7af45613782c5fda2262f78
Andreas Mattes <andreasmattes@fastmail.de>
2021-05-11 18:07:16 +0200

    TPRUN-733 Upgrades of CXF to 3.3.10 and ActiveMQ to 5.15.15.

--- request-callback/src/main/java/org/talend/esb/mep/requestcallback/impl/wsdl/RequestCallbackWSDLExtensionLoader.java
+++ request-callback/src/main/java/org/talend/esb/mep/requestcallback/impl/wsdl/RequestCallbackWSDLExtensionLoader.java
@@ -13,23 +13,23 @@ import org.apache.cxf.wsdl.WSDLManager;
 @NoJSR250Annotations
 public class RequestCallbackWSDLExtensionLoader implements WSDLExtensionLoader {
 
-	private static final Class<?>[][] EXTENSORS = new Class[][] {
-		{Binding.class, CallbackExtension.class},
-		{Definition.class, PLType.class}
+    private static final Class<?>[][] EXTENSORS = new Class[][] {
+        {Binding.class, CallbackExtension.class},
+        {Definition.class, PLType.class}
     };
 
     public RequestCallbackWSDLExtensionLoader(Bus bus) {
-    	WSDLManager manager = bus.getExtension(WSDLManager.class);
+        WSDLManager manager = bus.getExtension(WSDLManager.class);
         for (Class<?>[] ext : EXTENSORS) {
-            addExtensions(manager, ext[0], ext[1]);
+            addExtensions(bus, manager, ext[0], ext[1]);
         }
     }
 
-    public void addExtensions(WSDLManager manager, Class<?> parentType, Class<?> elementType) {
+    public void addExtensions(Bus bus, WSDLManager manager, Class<?> parentType, Class<?> elementType) {
         try {
-            JAXBExtensionHelper.addExtensions(
-            		manager.getExtensionRegistry(),
-            		parentType, elementType, null,
+            JAXBExtensionHelper.addExtensions(bus,
+                    manager.getExtensionRegistry(),
+                    parentType, elementType, null,
                     getClass().getClassLoader());
         } catch (JAXBException e) {
             // ignore, won't support XML
